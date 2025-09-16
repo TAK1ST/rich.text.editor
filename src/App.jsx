ecb7c122e8b739e4874bd5027e5d88d624bcae9e
KIETTA-TAK <trananhkiet21082005@gmail.com>
2025-09-17 00:12:19 +0700

    feat: create rich text template

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,161 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+  ClassicEditor,
+  Essentials,
+  Paragraph,
+  Bold,
+  Code,
+  Italic,
+  Strikethrough,
+  Subscript,
+  Superscript,
+  Underline,
+  Autoformat,
+  TextTransformation,
+  Autosave,
+  Indent,
+  IndentBlock,
+  BlockQuote,
+  Bookmark,
+  Minimap,
+  Clipboard,
+  Emoji,
+  Mention,
+  Font,
+  FindAndReplace,
+  Fullscreen,
+  Heading,
+  Image,
+  ImageCaption,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  LinkImage,
+  Alignment,
+  List,
+} from "ckeditor5";
+
+import "ckeditor5/ckeditor5.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CKEditor
+      editor={ClassicEditor}
+      config={{
+        licenseKey: "GPL",
+        plugins: [
+          List,
+          Image,
+          ImageToolbar,
+          ImageCaption,
+          ImageStyle,
+          ImageResize,
+          LinkImage,
+          Heading,
+          Fullscreen,
+          FindAndReplace,
+          Font,
+          Essentials,
+          Paragraph,
+          Bold,
+          Code,
+          Italic,
+          Strikethrough,
+          Subscript,
+          Superscript,
+          Underline,
+          Autoformat,
+          TextTransformation,
+          Autosave,
+          Indent,
+          IndentBlock,
+          BlockQuote,
+          Bookmark,
+          Minimap,
+          Clipboard,
+          Emoji,
+          Mention,
+          Alignment,
+        ],
+        toolbar: [
+          "fullscreen",
+          "Bookmark",
+          "|",
+          "undo",
+          "redo",
+          "|",
+          "heading",
+          "fontSize",
+          "fontFamily",
+          "fontColor",
+          "fontBackgroundColor",
+          "|",
+          "bold",
+          "italic",
+          "Underline",
+          "Code",
+          "|",
+          "bulletedList",
+          "numberedList",
+          "|",
+          "BlockQuote",
+          "insertImage",
+          "|",
+          "alignment",
+          "indent",
+          "outdent",
+          "|",
+          "emoji",
+        ],
+        initialData: "<p>Hello from CKEditor 5 in React!</p>",
+        //configuration for autosave
+        autosave: {
+          waitingTime: 5000,
+          save(editor) {}, //Need Config More
+        },
+        indentBlock: {
+          offset: 1,
+          unit: "em",
+        },
+        //configuration for minimap
+        minimap: {
+          // Configuration.
+        },
+        fontFamily: {
+          // Configuration.
+        },
+        fontColor: {
+          // Configuration.
+        },
+        findAndReplace: {
+          uiType: "dropdown",
+        },
+        fullscreen: {
+          menuBar: {
+            isVisible: false,
+          },
+        },
+        image: {
+          insert: {
+            // This is the default configuration, you do not need to provide
+            // this configuration key if the list content and order reflects your needs.
+            integrations: ["upload", "assetManager", "url"],
+            type: "auto",
+          },
+          toolbar: [
+            "imageStyle:block",
+            "imageStyle:side",
+            "|",
+            "toggleImageCaption",
+            "imageTextAlternative",
+            "|",
+            "linkImage",
+          ],
+        },
+      }}
+    />
+  );
 }
 
-export default App
+export default App;
